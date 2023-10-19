5101259fdfcd09389086f3e367bb5365b401a8a1
Deva Pratama <39708992+vedasaa@users.noreply.github.com>
2023-10-19 21:42:19 +0700

    Add files via upload

---
+++ cekfeewd.py
@@ -0,0 +1,35 @@
+import random
+"""
+    vedpratama x SGBTEAM
+    vedasaa
+"""
+import requests
+import json
+import os
+import time
+import re
+import html
+import socket
+import struct
+
+def checking():
+    url = 'https://api.trustlessbridge.io/api/estimate-withdraw-fee'
+
+    h={
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46"
+    }
+    result = requests.post(url, json={"isBridgeLayer": False,"tcAddress":"0x60992096FfA675eb58ceDb4536C88259C5983f14","tcTokenID":"0x111808abe352c8003e0effcc04998eab26cebe3c"}, headers=h)
+    return result
+
+token = '6500087622:AAEpWkKybAcDhzeV685-Ht88njub6biTtbw'
+userID = '648235175'
+tele = f'https://api.telegram.org/bot{token}/sendMessage'
+while True:
+    #result = float(checking().json()["data"]["withdrawFee"])/1000000000000000000
+    print(checking().text)
+    target = 20000000000000/1000000000000000000
+    
